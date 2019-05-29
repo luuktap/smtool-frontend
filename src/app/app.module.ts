c4045a5edda62f77c2f19a1bc6ecc1f3b14f6c9a
Luuk Tap <luuktap@gmail.com>
2019-05-29 09:05:05 +0200

    Prettyfied app component & added user interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,65 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { LayoutModule } from '@angular/cdk/layout';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  BrowserModule
+} from '@angular/platform-browser';
+import {
+  NgModule,
+  APP_INITIALIZER
+} from '@angular/core';
+import {
+  LayoutModule
+} from '@angular/cdk/layout';
+import {
+  BrowserAnimationsModule
+} from '@angular/platform-browser/animations';
+import {
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms';
+import {
+  HttpClientModule
+} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import {
+  AppRoutingModule
+} from './app-routing.module';
 
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
+import {
+  AppComponent
+} from './app.component';
+import {
+  NavComponent
+} from './nav/nav.component';
 
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule, MatSelectModule, MatRadioModule } from '@angular/material';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UsermanagementComponent } from './usermanagement/usermanagement.component';
-import { LoginComponent } from './login/login.component';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatMenuModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatInputModule,
+  MatSelectModule,
+  MatRadioModule,
+  MatProgressSpinnerModule
+} from '@angular/material';
+import {
+  DashboardComponent
+} from './dashboard/dashboard.component';
+import {
+  UsermanagementComponent
+} from './usermanagement/usermanagement.component';
+import {
+  LoginComponent
+} from './login/login.component';
+
+import {
+  AuthService
+} from './services/auth.service';
 
 @NgModule({
   declarations: [
@@ -41,10 +88,21 @@ import { LoginComponent } from './login/login.component';
     MatInputModule,
     MatSelectModule,
     MatRadioModule,
+    MatProgressSpinnerModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: APP_INITIALIZER,
+    useFactory: startApp,
+    deps: [AuthService],
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
+export function startApp(auth: AuthService) {
+  return () => auth.requestUser();
+}
