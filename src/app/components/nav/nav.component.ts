4ad6dc5e7e52eddfa81c73d5156336cbcdb3190a
Luuk Tap <luuktap@gmail.com>
2019-07-12 02:12:30 +0200

    migrated nav component

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable, Subscriber, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { MatMenu } from '@angular/material/menu';
+import { MatSidenav } from '@angular/material/sidenav';
+
+import { User } from 'src/app/interfaces/user';
+import { NavRoute } from 'src/app/interfaces/nav-route';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
 
-  constructor() { }
+export class NavComponent {
 
-  ngOnInit() {
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private AuthService: AuthService
+  ) {}
+
+  routes: NavRoute[] = [{
+    icon: 'dashboard',
+    route: '/dashboard',
+    title: 'Dashboard'
+  },
+  {
+    icon: 'account_circle',
+    route: '/user-management',
+    title: 'User Management'
+  },
+  {
+    icon: 'account_circle',
+    route: '/login',
+    title: 'Login'
+  }];
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      map(result => this.isHandset = result)
+    );
+  
+  isHandset: boolean;
+  @ViewChild('drawer', { static: false }) drawer: MatSidenav;
+
+  isAuthenticated: boolean = this.AuthService.isAuthenticated()
+
+  closeDrawerIfPhone() {
+    if(this.isHandset) {
+      this.drawer.close()
+    }
   }
-
 }
