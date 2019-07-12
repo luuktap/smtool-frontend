4472af9e5baf551cba43c6c57888032bd907826a
Luuk Tap <luuktap@gmail.com>
2019-07-12 02:40:16 +0200

    remove / which lead to incorrect routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,11 +18,11 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: '/dashboard'
+    redirectTo: 'dashboard'
   },
   {
     path: '**',
-    redirectTo: '/dashboard'
+    redirectTo: 'dashboard'
   }
 ];
 
